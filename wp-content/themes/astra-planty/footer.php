d9c38931d68a568bddf510dabcef32161ef0c013
Alexandra Maligne <alexandra.maligne@gmail.com>
2023-02-17 10:44:43 +0100

    Creation des pages web

---
+++ wp-content/themes/astra-planty/footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+	</div><!-- #page -->
+
+<!-- ajout du footer personnalisé --> 
+<footer class="site-footer">
+	<a href="mentions-legales">Mentions légales</a>
+</footer>
+
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+	</body>
+</html>
